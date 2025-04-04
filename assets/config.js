6be9ceb367ba60a2879d45de67c93c937eefb5b8
Keelhauler <41901381+Keelhauler@users.noreply.github.com>
2025-04-03 19:52:50 -0700

    Update config.js
    updated links

--- assets/config.js
+++ assets/config.js
@@ -4,44 +4,19 @@ let config = {
   "Description": "Cars, Tech, and Photography",
   "links": [
     {
-      "Title": "Website",
-      "URL": "http://www.harindu.dev",
-      "icon_classes": "fas fa-link",
+      "Title": "Instagram",
+      "URL": "https://www.instagram.com/the.a90.guy",
+      "icon_classes": "fas fa-instagram",
     },
     {
       "Title": "Github",
-      "URL": "https://github.com/harindulk",
+      "URL": "https://github.com/keelhauler",
       "icon_classes": "fab fa-github",
     },
     {
-      "Title": "Play Store",
-      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
-      "icon_classes": "fab fa-google-play"
-    },
-    {
-      "Title": "PC Games",
-      "URL": "https://harindulk.itch.io/",
-      "icon_classes": "fab fa-itch-io"
-    },
-    {
-      "Title": "Blog",
-      "URL": "blog-test",
-      "icon_classes": "fas fa-blog"
-    },
-    {
-      "Title": "Twitter",
-      "URL": "https://twitter.com/Harindu_Fonseka",
-      "icon_classes": "fab fa-twitter"
-    },
-    {
-      "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/harindulk/",
-      "icon_classes": "fab fa-linkedin"
-    },
-    {
-      "Title": "Youtube",
-      "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
-      "icon_classes": "fab fa-youtube"
+      "Title": "Supra Car Build",
+      "URL": "https://carbuild.co/c/2022-toyota-supra-1",
+      "icon_classes": "fab fa-car"
     },
   ]
 }
